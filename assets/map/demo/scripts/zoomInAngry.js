82242e7f3749e68084b1a80cab4ef1e73b942c4f
Dominik Madarász <zaklaus@outlook.com>
2018-12-13 13:45:04 +0100

    Update zoomInAngry.js

--- assets/map/demo/scripts/zoomInAngry.js
+++ assets/map/demo/scripts/zoomInAngry.js
@@ -3,6 +3,6 @@ var cam = MainCamera
 cam.SetCameraMode("follow")
 cam.TargetZoom = 4.0
 cam.ZoomSpeed = 0.9
-setProperty(cam, "Follow", findObject("player"))
+setProperty(cam, "Follow", LocalPlayer)
 cam.Speed = 0.06
 cam.Visible = false

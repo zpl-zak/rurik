0b11183163c326d725c812c0e4444275ebaafbcc
Dominik Madarász <zaklaus@outlook.com>
2019-01-01 00:10:23 +0100

    Happy New Year!

--- src/core/core.go
+++ src/core/core.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 V4 Games
+   Copyright 2019 V4 Games
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

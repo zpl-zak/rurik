7e39ffbd5e6007370b19a087f5d15127e9bbda11
Dominik Madarász <zaklaus@outlook.com>
2019-03-13 14:59:09 +0100

    Copyright changes

--- src/core/savegame.go
+++ src/core/savegame.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019 V4 Games
+   Copyright 2019 Dominik Madarász <zaklaus@madaraszd.net>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

bfe9cf1e0a7631d962a5dbd651f5babc5e6903b5
Dominik Madarász <zaklaus@outlook.com>
2019-04-29 23:07:03 +0200

    Update shipping script

--- zip.sh
+++ zip.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir package 2> /dev/null
-cp -Rf assets package/assets 
+cp -Rf data package/data 
 cp build/rurik.exe package/${1-game.exe} -f
 
 cd package
-7za a build.zip assets/ ${1-game.exe} -bso0 -bsp0 -tzip -mx7 -xr!*.aseprite
+7za a build.zip data/ ${1-game.exe} -bso0 -bsp0 -tzip -mx7 -xr!*.aseprite
 cd ..

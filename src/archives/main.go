c376e9103bef19c8b61f0c24330e3bd55d8d1e45
Dominik Madarász <zaklaus@outlook.com>
2019-05-13 04:17:24 +0200

    Small changes

--- src/archives/main.go
+++ src/archives/main.go
@@ -27,6 +27,32 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	checked := rl.GenImageChecked(32, 32, 1, 1, rl.Black, rl.White)
+	tex := rl.LoadTextureFromImage(checked)
+	rl.UnloadImage(checked)
+
+	cam := rl.NewCamera3D(
+		rl.NewVector3(5, -5, 5),
+		rl.NewVector3(0, 0, 0),
+		rl.NewVector3(0, 1, 0),
+		62,
+		0,
+	)
+
+	modelsnap := system.CreateRenderTarget(screenW, screenH)
+
+	rl.BeginTextureMode(modelsnap)
+	{
+		rl.BeginMode3D(cam)
+		{
+			rl.ClearBackground(rl.Blank)
+			rl.DrawCubeTexture(tex, rl.Vector3{}, 4, 4, 4, rl.White)
+			rl.DrawGrid(10, 1)
+		}
+		rl.EndMode3D()
+	}
+	rl.EndTextureMode()
+
 	avatar := system.GetTexture("gfx/avatar.png")
 
 	for !rl.WindowShouldClose() {
@@ -34,6 +60,10 @@ func main() {
 
 		rl.ClearBackground(rl.RayWhite)
 
+		rl.DrawTexturePro(modelsnap.Texture,
+			rl.NewRectangle(0, 0, screenW, screenH),
+			rl.NewRectangle(0, 0, screenW, screenH), rl.Vector2{}, 0,
+			rl.White)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 		rl.DrawTexturePro(
 			*avatar,
@@ -45,6 +75,7 @@ func main() {
 		)
 
 		rl.EndDrawing()
+		rl.PollInputEvents()
 	}
 
 	rl.CloseWindow()
